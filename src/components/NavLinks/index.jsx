e195cbbd92b90e78ce33346f9605f70af9e0da62
ldickmann <ldickmann12@gmail.com>
2025-03-22 02:04:08 -0300

    STYLE: Componente NavLinks

--- src/components/NavLinks/index.jsx
+++ src/components/NavLinks/index.jsx
@@ -1,80 +1,116 @@
-import { Link, useLocation } from "react-router-dom";
-import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 
-const StyledLink = styled(Link)`
+const LinkText = styled.div`
+  font-size: 1.2rem;
+  text-align: center;
+  position: relative;
+  z-index: 2;
   color: var(--color-green);
   text-decoration: none;
-  padding: 1rem 0.5rem;
-  border-radius: 3px;
-  transition: background-color 0.3s ease, color 0.3s ease, padding 0.3s ease;
-  font-size: 1.4rem;
+  font-family: "Black Ops One", "Righteous", Arial, sans-serif;
+  text-transform: uppercase;
+  transition: 0.3s ease-in-out;
+  -webkit-transition: 0.3s ease-in-out;
+  transform: translate(0, 0);
+  -webkit-transform: translate(0, 0);
+  text-shadow: rgba(133, 185, 255, 0);
 
-  &:hover {
-    background-color: var(--color-white-medium);
-    color: var(--color-black-medium);
-    display: flex;
-    height: 30%;
+  &:after {
+    display: inline-block;
+    content: attr(data-text);
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    transition: 0.3s ease-in-out;
+    -webkit-transition: 0.3s ease-in-out;
+    opacity: 0;
+    transform: translate(-20px, -50px) rotate(-20deg);
+    -webkit-transform: translate(-20px, -50px) rotate(-20deg);
+    color: var(--color-white);
+    max-width: 50%;
+    overflow: hidden;
+    visibility: hidden;
   }
-
-  &:active {
-    background-color: var(--color-beige);
-    color: var(--color-white-medium);
-  }
-
-  ${({ $isActive }) =>
-    $isActive &&
-    css`
-      color: var(--color-green-active);
-    `}
 `;
 
-const SidebarLink = styled(Link)`
-  color: white;
-  font-family: "Silkscreen", serif;
-  font-weight: 500;
-  font-size: 1.5rem;
-  width: 90%;
-  transition: opacity 0.3s ease, color 0.3s ease, padding 0.3s ease;
+const TestBox = styled.div`
+  position: absolute;
+  transition: 0.3s ease-in-out;
+  -webkit-transition: 0.3s ease-in-out;
+  left: 50%;
+  height: 25px;
+  width: 100%;
+  top: 15px;
+  background: transparent;
+  text-align: right;
+  overflow: hidden;
+  transform: translate(20px, -50px) rotate(20deg);
+  -webkit-transform: translate(20px, -50px) rotate(20deg);
+  z-index: 2;
+  opacity: 0;
+  transition-delay: 0.15s;
+  -webkit-transition-delay: 0.15s;
+  visibility: visible;
+
+  span {
+    font-size: 1.2rem;
+    position: absolute;
+    left: -50%;
+    z-index: 2;
+    color: var(--color-white);
+    text-decoration: none;
+    font-family: "Black Ops One", "Righteous", Arial, sans-serif;
+    text-transform: uppercase;
+    transition: 0.3s ease-in-out;
+    -webkit-transition: 0.3s ease-in-out;
+  }
+`;
+
+const NavStyleItem = styled(Link)`
+  position: relative;
+  display: inline-block;
+  height: 35px;
+  padding-top: 15px;
+  margin: 0 5px;
+  background: transparent;
+  z-index: 2;
+  color: #007bff;
   text-decoration: none;
+  font-family: Arial, sans-serif;
+  text-transform: uppercase;
+  transition: 0.3s ease-in-out;
+  -webkit-transition: 0.3s ease-in-out;
 
-  &:hover {
-    opacity: 0.8;
-    color: var(--color-white-medium);
-    padding: 3.2rem 0.7rem;
+  &:hover ${LinkText}:after, &:hover ${TestBox} {
+    visibility: visible;
+    transform: translate(0, 0);
+    -webkit-transform: translate(0, 0);
+    opacity: 1;
   }
 
-  &:active {
-    opacity: 0.6;
-    color: var(--color-green-active);
+  &:hover ${LinkText} {
+    color: rgba(133, 185, 255, 0);
+    transition-delay: 0.3s;
+    -webkit-transition-delay: 0.3s;
   }
-
-  ${({ $isActive }) =>
-    $isActive &&
-    css`
-      color: var(--color-green-active);
-    `}
 `;
 
-const NavLinks = ({ children, to, isSidebar }) => {
-  const location = useLocation();
-  const isActive = location.pathname === to;
-
-  return isSidebar ? (
-    <SidebarLink to={to} $isActive={isActive}>
-      {children}
-    </SidebarLink>
-  ) : (
-    <StyledLink to={to} $isActive={isActive}>
-      {children}
-    </StyledLink>
+const NavLinks = ({ children, to }) => {
+  return (
+    <NavStyleItem to={to}>
+      <LinkText data-text={children}>{children}</LinkText>
+      <TestBox>
+        <span>{children}</span>
+      </TestBox>
+    </NavStyleItem>
   );
 };
 
 NavLinks.propTypes = {
   children: PropTypes.node.isRequired,
   to: PropTypes.string.isRequired,
-  isSidebar: PropTypes.bool,
 };
 
 export default NavLinks;
