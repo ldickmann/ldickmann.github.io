eb263cfe2f0ace56a96546a503aa21b2096b185d
ldickmann <ldickmann12@gmail.com>
2025-05-03 07:38:54 -0300

    STYLES: Footer add dividerComponente e retirada algumas classes css desnecessarias

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,43 @@
 import styled from "styled-components";
+import DividerComponent from "../DividerComponent";
 
 const FooterStyles = styled.footer`
   /* background-color: var(--color-black); */
   color: var(--color-white);
   padding: 1rem 3rem;
   text-align: center;
-  font-size: 1.1rem;
+  font-size: 1rem;
+
+  p {
+    margin: 0 1rem;
+  }
+`;
+
+const FooterDivider = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 `;
 
 const Footer = () => {
   return (
     <FooterStyles>
-      <p>Copyright &copy; 2025 | Developer Lucas Elias Dickmann</p>
+      <FooterDivider>
+        <DividerComponent
+          orientation="horizontal"
+          width="40%"
+          height="1px"
+          variant="solid"
+        />
+        <p>Copyright &copy; 2025 | Developer Lucas Elias Dickmann</p>
+        <DividerComponent
+          orientation="horizontal"
+          width="40%"
+          height="1px"
+          variant="solid"
+        />
+      </FooterDivider>
     </FooterStyles>
   );
 };
