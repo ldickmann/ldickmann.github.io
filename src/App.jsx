31dcb0024ae7cb6a6e1fe787195fd1cf321dd8ac
ldickmann <ldickmann12@gmail.com>
2025-05-06 23:54:30 -0300

    REFACTOR: Remoção de códigos comentados em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { useState } from "react";
-// import PropTypes from "prop-types";
-// import Navbar from "./components/Navbar";
-// import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
-// import Projects from "./pages/Projects";
 import Footer from "./components/Footer";
-
-// function App() {
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
-
-//   const toggleSidebar = () => {
-//     setSidebarOpen(!sidebarOpen);
-//   };
-
-//   return (
-//     <BrowserRouter basename="/">
-//       <AppContent
-//         toggleSidebar={toggleSidebar}
-//         sidebarOpen={sidebarOpen}
-//         setSidebarOpen={setSidebarOpen}
-//       />
-//     </BrowserRouter>
-//   );
-// }
-
-// function AppContent({ toggleSidebar, sidebarOpen, setSidebarOpen }) {
-//   const location = useLocation();
-//   const isParticlesRoute = location.pathname === "/";
-
-//   return (
-//     <>
-//       {!isParticlesRoute && (
-//         <Navbar
-//           toggleSidebar={toggleSidebar}
-//           isSidebarOpen={sidebarOpen}
-//         />
-//       )}
-//       <Sidebar
-//         isOpen={sidebarOpen}
-//         setIsOpen={setSidebarOpen}
-//       />
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={<Home toggleSidebar={toggleSidebar} />}
-//         />
-//         <Route
-//           path="/projects"
-//           element={<Projects />}
-//         />
-//         <Route
-//           path="*"
-//           element={<div>Página não encontrada...</div>}
-//         />
-//       </Routes>
-//       <Footer />
-//     </>
-//   );
-// }
 
 function App() {
   return (
@@ -79,11 +21,5 @@ function App() {
     </BrowserRouter>
   );
 }
-
-// AppContent.propTypes = {
-//   toggleSidebar: PropTypes.func.isRequired,
-//   sidebarOpen: PropTypes.bool.isRequired,
-//   setSidebarOpen: PropTypes.func.isRequired,
-// };
 
 export default App;
