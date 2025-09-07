08cc248d04895c4c5e8be6c441d33ee540bb88cf
ldickmann <ldickmann12@gmail.com>
2025-09-07 20:06:29 -0300

    feat: adicionar seção de contato ao rodapé e implementar lógica de envio de formulário *em implementação*

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-// filepath: /c:/Users/ldick/repositorios_github/newPortfolio/vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 3000,
+  },
 });
